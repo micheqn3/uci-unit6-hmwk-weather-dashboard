9465d63b6abd12785ca91d2b6fddc62e225cf559
Michelle Nguyen <michellenguyen11239@aol.com>
2021-05-10 14:13:21 -0700

    Refactored script.js

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
-var searchBtn = $(".searchBtn"); // Targets the search button 
-var inputForm = $(".inputForm"); // Targets the input box 
-var citySpan = $(".citySpan"); // Targets the location for city
-var dateSpan = $(".dateSpan"); // Targets the location for current date 
-var tempSpan = $(".tempSpan"); // Targets the location for temperature
-var humiditySpan = $(".humiditySpan"); // Targets the location for temperature
-var windSpan = $(".windSpan"); // Targets the location for temperature
-var uvSpan = $(".uvSpan"); // Targets the location for temperature
-var day1 = $(".day1"); // Targets day 1 card
-var day1Icon = $(".day1Icon");
-var day1Temp = $(".day1Temp");
-var day1Humidity = $(".day1Humidity"); 
-var day2 = $(".day2"); // Targets day 2 card
-var day2Icon = $(".day2Icon");
-var day2Temp = $(".day2Temp");
-var day2Humidity = $(".day2Humidity");
-var day3 = $(".day3"); // Targets day 3 card
-var day3Icon = $(".day3Icon");
-var day3Temp = $(".day3Temp");
-var day3Humidity = $(".day3Humidity");
-var day4 = $(".day4"); // Targets day 4 card
-var day4Icon = $(".day4Icon");
-var day4Temp = $(".day4Temp");
-var day4Humidity = $(".day4Humidity");
-var day5 = $(".day5"); // Targets day 5 card
-var day5Icon = $(".day5Icon");
-var day5Temp = $(".day5Temp");
-var day5Humidity = $(".day5Humidity");
-var welcomeText = $(".welcomeText"); // Targets the welcome title
-var containerBlock = $(".containerBlock"); // Targets the container holding the 5 day forecast
-var card = $(".card") // Targets the main card holding the city/date info
-var historyList = $(".history-list") // Targets the search history
-var clearBtnLocation = $(".clear-btn-location")
+let day1 = $(".day1"); // Targets day 1 card
+let day1Icon = $(".day1Icon");
+let day1Temp = $(".day1Temp");
+let day1Humidity = $(".day1Humidity"); 
+let day2 = $(".day2"); // Targets day 2 card
+let day2Icon = $(".day2Icon");
+let day2Temp = $(".day2Temp");
+let day2Humidity = $(".day2Humidity");
+let day3 = $(".day3"); // Targets day 3 card
+let day3Icon = $(".day3Icon");
+let day3Temp = $(".day3Temp");
+let day3Humidity = $(".day3Humidity");
+let day4 = $(".day4"); // Targets day 4 card
+let day4Icon = $(".day4Icon");
+let day4Temp = $(".day4Temp");
+let day4Humidity = $(".day4Humidity");
+let day5 = $(".day5"); // Targets day 5 card
+let day5Icon = $(".day5Icon");
+let day5Temp = $(".day5Temp");
+let day5Humidity = $(".day5Humidity");
 
 $( document ).ready(function() {
-    containerBlock.hide(); // Hide the forecast container on page load
-    card.hide(); // Hide the city name/date on page load
+    $(".containerBlock").hide(); // Hide the forecast container on page load
+    $(".card").hide(); // Hide the city name/date on page load
 });
 
 function getUserInput(event) { // This function retrieves user input and creates a url endpoint with that info
     event.preventDefault();
     console.log("Button clicked works"); // Checking if button was clicked
-    var userInput = inputForm.val().trim(); // Saves the user's input and removes whitespace
-    inputForm.val(""); // Clears the input form 
+    let userInput = $(".inputForm").val().trim(); // Saves the user's input and removes whitespace
+    $(".inputForm").val(""); // Clears the input form 
 
     if (userInput) { // Checking if the user entered something
         console.log(userInput); // Checking if user's input was saved 
-        var cityUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=bcb03c4915d5966190a74c259d6834d8";
+        let enc = "YmNiMDNjNDkxNWQ1OTY2MTkwYTc0YzI1OWQ2ODM0ZDg=";
+        let dec = window.atob(enc);
+        let cityUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=" + dec;
         appendItems(cityUrl);   // Start the appendItems function with the user input. API parameter = imperial for fahrenheit
     } else {
         alert("Please enter in a city.");  // Validation for if the user doesn't enter in anything 
@@ -66,61 +55,61 @@ function appendItems(cityUrl) {
         })
         .then(function(data) { // Do this to the data
             console.log(data);
-            welcomeText.hide(1000); // Hide the welcome text
-            containerBlock.show(2000); // Show the forecast container
-            card.show(); // Show the city name/date
-            var cityName = data.city.name; // Accesses the city's name 
-            var country = data.city.country; // Accesses the country 
-            var date = moment.unix(data.list[0].dt).format("MMMM Do"); // Converts to current date in city
-            var temp = data.list[0].main.temp; // Temperature in fah. Can use this again for day 1 temp below.
-            var humidity = data.list[0].main.humidity; // Humidity in %
-            var wind = data.list[0].wind.speed; // Wind speed in m/hour
-            citySpan.text(`${cityName}, ${country}`); // Filling the spans with the data
-            dateSpan.text(date);
-            tempSpan.text(temp);
-            humiditySpan.text(humidity);
-            windSpan.text(wind);
+            $(".welcomeText").hide(1000); // Hide the welcome text
+            $(".containerBlock").show(2000); // Show the forecast container
+            $(".card").show(); // Show the city name/date
+            let cityName = data.city.name; // Accesses the city's name 
+            let country = data.city.country; // Accesses the country 
+            let date = moment.unix(data.list[0].dt).format("MMMM Do"); // Converts to current date in city
+            let temp = data.list[0].main.temp; // Temperature in fah. Can use this again for day 1 temp below.
+            let humidity = data.list[0].main.humidity; // Humidity in %
+            let wind = data.list[0].wind.speed; // Wind speed in m/hour
+            $(".citySpan").text(`${cityName}, ${country}`); // Filling the spans with the data
+            $(".dateSpan").text(date);
+            $(".tempSpan").text(temp);
+            $(".humiditySpan").text(humidity);
+            $(".windSpan").text(wind);
 
             /* Goes through 0, 8, 16, 24, 32 for next day's data */ 
-            var icon1 = data.list[0].weather[0].icon; // Day 1 Forecast
-            var iconUrl1 = "https://openweathermap.org/img/w/" + icon1 + ".png";
+            let icon1 = data.list[0].weather[0].icon; // Day 1 Forecast
+            let iconUrl1 = "https://openweathermap.org/img/w/" + icon1 + ".png";
             day1.text("TODAY"); // Same as the main card
             day1Icon.attr("src", iconUrl1);
             day1Temp.text(temp); // Same as the main card
 
-            var icon2 = data.list[8].weather[0].icon; // Day 2 Forecast
-            var iconUrl2 = "https://openweathermap.org/img/w/" + icon2 + ".png";
-            var date2 = moment.unix(data.list[8].dt).format("dddd");
+            let icon2 = data.list[8].weather[0].icon; // Day 2 Forecast
+            let iconUrl2 = "https://openweathermap.org/img/w/" + icon2 + ".png";
+            let date2 = moment.unix(data.list[8].dt).format("dddd");
             day2.text(date2);
             day2Icon.attr("src", iconUrl2);
-            var date2Temp = data.list[8].main.temp; 
+            let date2Temp = data.list[8].main.temp; 
             day2Temp.text(date2Temp);
 
-            var icon3 = data.list[16].weather[0].icon; // Day 3 Forecast
-            var iconUrl3 = "https://openweathermap.org/img/w/" + icon3 + ".png";
-            var date3 = moment.unix(data.list[16].dt).format("dddd");
+            let icon3 = data.list[16].weather[0].icon; // Day 3 Forecast
+            let iconUrl3 = "https://openweathermap.org/img/w/" + icon3 + ".png";
+            let date3 = moment.unix(data.list[16].dt).format("dddd");
             day3.text(date3);
             day3Icon.attr("src", iconUrl3);
-            var date3Temp = data.list[16].main.temp; 
+            let date3Temp = data.list[16].main.temp; 
             day3Temp.text(date3Temp);
 
-            var icon4 = data.list[24].weather[0].icon; // Day 4 Forecast
-            var iconUrl4 = "https://openweathermap.org/img/w/" + icon4 + ".png";
-            var date4 = moment.unix(data.list[24].dt).format("dddd");
+            let icon4 = data.list[24].weather[0].icon; // Day 4 Forecast
+            let iconUrl4 = "https://openweathermap.org/img/w/" + icon4 + ".png";
+            let date4 = moment.unix(data.list[24].dt).format("dddd");
             day4.text(date4);
             day4Icon.attr("src", iconUrl4);
-            var date4Temp = data.list[24].main.temp; 
+            let date4Temp = data.list[24].main.temp; 
             day4Temp.text(date4Temp);
 
-            var icon5 = data.list[32].weather[0].icon; // Day 5 Forecast
-            var iconUrl5 = "https://openweathermap.org/img/w/" + icon5 + ".png";
-            var date5 = moment.unix(data.list[32].dt).format("dddd");
+            let icon5 = data.list[32].weather[0].icon; // Day 5 Forecast
+            let iconUrl5 = "https://openweathermap.org/img/w/" + icon5 + ".png";
+            let date5 = moment.unix(data.list[32].dt).format("dddd");
             day5.text(date5);
             day5Icon.attr("src", iconUrl5);
-            var date5Temp = data.list[32].main.temp; 
+            let date5Temp = data.list[32].main.temp; 
             day5Temp.text(date5Temp);
 
-            var storeObject = { // Saving the data into an object
+            let storeObject = { // Saving the data into an object
                 "cityName": cityName,
                 "country": country,
                 "date": date, 
@@ -141,36 +130,36 @@ function appendItems(cityUrl) {
                 "date5": date5,
                 "date5Temp": date5Temp,
             }
-            var items = localStorage.getItem("items");
+            let items = localStorage.getItem("items");
             if (items === null) {
                 items = [];
             } else {
                 items = JSON.parse(items);
             }
             items.push(storeObject); // Push the object into the array
-            var allItems = JSON.stringify(items);
+            let allItems = JSON.stringify(items);
             localStorage.setItem("items", allItems); // Save allItems into local storage
         });            
 };
 
 function showHistoryBtn() {
-    var items = localStorage.getItem("items");
+    let items = localStorage.getItem("items");
     items = JSON.parse(items);
 
     if (items !== null) {
-        var counter = items.length - 1; // This will count the actual number of items in the object
+        let counter = items.length - 1; // This will count the actual number of items in the object
 
-        for (var i = 0 ; i <= counter ; i++ ) {
-            var button = $("<button>");
+        for (let i = 0 ; i <= counter ; i++ ) {
+            let button = $("<button>");
             button.addClass("list-group-item");
             button.text(items[i].cityName);
             button.attr("name", items[i].cityName);
-            historyList.append(button);
+            $(".history-list").append(button);
         }
-        var buttontag = $("<button>"); // Create clear button 
+        let buttontag = $("<button>"); // Create clear button 
         buttontag.addClass("list-group-item clear-btn");
         buttontag.text("CLEAR ALL");
-        clearBtnLocation.append(buttontag);
+        $(".clear-btn-location").append(buttontag);
     } else {
         console.log("Empty local storage.");
     }
@@ -178,20 +167,20 @@ function showHistoryBtn() {
 
 function showHistoryData(e) {
     e.preventDefault;
-    welcomeText.hide(1000); // Show the welcome text
+    $(".welcomeText").hide(1000); // Show the welcome text
     $(".refresh").hide();
-    containerBlock.show(2000); // Show the forecast container
-    card.show(1000); // Show the main city/date card
-    var items = localStorage.getItem("items");
+    $(".containerBlock").show(2000); // Show the forecast container
+    $(".card").show(1000); // Show the main city/date card
+    let items = localStorage.getItem("items");
     items = JSON.parse(items);
-    var nameOfCity = e.target.getAttribute("name");
-    var obj =items.find(i => i.cityName === nameOfCity); //  Finds the array in the object that matches city name
+    let nameOfCity = e.target.getAttribute("name");
+    let obj =items.find(i => i.cityName === nameOfCity); //  Finds the array in the object that matches city name
 
-    citySpan.text(obj.cityName); // Fills the screen with the data of the city clicked on 
-    dateSpan.text(obj.date);
-    tempSpan.text(obj.temp);
-    humiditySpan.text(obj.humidity);
-    windSpan.text(obj.wind);
+    $(".citySpan").text(obj.cityName); // Fills the screen with the data of the city clicked on 
+    $(".dateSpan").text(obj.date);
+    $(".tempSpan").text(obj.temp);
+    $(".humiditySpan").text(obj.humidity);
+    $(".windSpan").text(obj.wind);
     day1.text("TODAY");
     day1Icon.attr("src", obj.iconUrl1);
     day1Temp.text(obj.temp); 
@@ -209,17 +198,17 @@ function showHistoryData(e) {
     day5Temp.text(obj.date5Temp);
 }
 
-clearBtnLocation.on("click", function() { // When the clear button is clicked, local storage + history list is cleared
+$(".clear-btn-location").on("click", function() { // When the clear button is clicked, local storage + history list is cleared
     console.log("Clear button clicked.");
     $(this).hide();
     localStorage.clear();
-    historyList.text("");
+    $(".history-list").text("");
     window.location.reload();
 })
 
 showHistoryBtn(); // Show the recent searches on page load
-searchBtn.on("click", getUserInput); // When the user clicks the search, this function will execute
-historyList.on("click", showHistoryData) // When the user clicks the history button, show data on screen
+$(".searchBtn").on("click", getUserInput); // When the user clicks the search, this function will execute
+$(".history-list").on("click", showHistoryData) // When the user clicks the history button, show data on screen
 
 
 
